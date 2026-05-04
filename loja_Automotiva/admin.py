faf2754f3ebcaa2c17bd30af1290d7506bb4b461
IuriAllexander <iuriallexander17@gmail.com>
2026-05-04 11:12:41 -0300

    Atualização do projeto de python

--- loja_Automotiva/admin.py
+++ loja_Automotiva/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Categoria, Fabricante, Personalização, loja_Automotiva
 # Register your models here.
+ 
+admin.site.register(Categoria)
+admin.site.register(Fabricante)
+admin.site.register(Personalização)
+admin.site.register(loja_Automotiva)
--- loja_Automotiva/models.py
+++ loja_Automotiva/models.py
@@ -1,13 +1,37 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
 class Categoria (models.Model):
-    pass 
+    nome = models.CharField(max_length=150, unique=True)
+    descricao = models.TextField(blank=True, null=True)
+    codigo = models.CharField(max_length=50, unique=True)
 
+class Meta:
+    verbose_name = 'Categoria',
+    verbose_name_plural = 'Categorias',
+    ordering = ['nome']
+
+
+    def __str__(self):
+        return self.nome
 
 class Fabricante(models.Model):
-    pass
+    nome = models.CharField(max_length=150, unique=True)
+    pais_origem = models.CharField(max_length=100)
+    ano_fundacao = models.IntegerField()
+
+class Meta:
+    verbose_name = 'Fabricante',
+    verbose_name_plural = 'Fabricantes',
+    ordering = ['nome']
+
+
+    def __str__(self):
+        return self.nome
+
+        
 
 
 class Personalização(models.Model):
@@ -15,9 +39,52 @@ class Personalização(models.Model):
     escapamento = models.TextField(max_length=150)
     espelho = models.CharField(max_length=150)
 
+class Meta:
+    verbose_name = 'Personalização',
+    verbose_name_plural = 'Personalizações',
+    ordering = ['nome']
+
+
     def __str__(self):
         return self.rodas
 
 
 class loja_Automotiva(models.Model):
-    pass
+    nome = models.CharField('Loja', max_length=150, unique=True)
+    endereco = models.CharField('Endereço', max_length=200)
+    telefone = models.CharField('Telefone', max_length=20)
+
+
+
+    categoria = models.ForeignKey(
+        Categoria,
+        on_delete=models.PROTECT,
+        related_name='loja_Automotiva',
+        verbose_name='Categoria',
+    )
+
+    fabricante = models.ForeignKey(
+        Fabricante,
+        on_delete=models.PROTECT,
+        related_name='loja_Automotiva',
+        verbose_name='Fabricante',
+    )
+
+    personalização = models.ForeignKey(
+        Personalização,
+        on_delete=models.PROTECT,
+        related_name='loja_Automotiva',
+        verbose_name='Personalização',
+    )
+
+class Meta:
+    verbose_name = 'loja_Automotiva',
+    verbose_name_plural = 'loja_Automotivas'
+    ordering = ['nome']
+
+    def __str__(self):
+        return self.nome
+
+    
+    def get_absolute_url(self):
+        return reverse('loja_Automotiva', kwargs={'pk': self.pk})
