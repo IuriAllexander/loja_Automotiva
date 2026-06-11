376cd58b223ae6009030f7858940c0c9f4f1c479
isabele-eduarda <isabeleeduarda307@gmail.com>
2026-06-11 13:42:52 -0300

    feat: views home feita, adição de produtos na pagina

--- loja_Automotiva/views.py
+++ loja_Automotiva/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 #from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from .models import Produto
 
 
 def home(request):
-    # Render takes: request, template path, and optional context
-    return render(request, "home/index.html")
+    produtos = Produto.objects.filter(estoque__gt=1).order_by('preco')[:3]
+    contexto = {'produtos': produtos}
+    return render(request, "index.html", contexto)
